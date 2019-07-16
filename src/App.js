67ff033d96e38dac9adf1eb73db3d139011beb9f
beigenut <beigenut@gmail.com>
2019-07-16 00:38:44 -0400

    Learn React Hook. Read comments

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Recipe from './Recipe'
 import './index.css';
 
 const App = () => {
-  const APP_ID = "b56ee0f5";
-  const APP_KEY = "eeac782b73b58fa77e2aabd7bcb282d4";
+  // React Hook!
+  // In a function component, we have no this, so we can’t assign or read this.state. 
+  // Instead, we call the useState Hook directly inside our component:
+  // const [count, setCount] = useState(0);
+  // this.state.count = count   
+  // onClick={() => this.setState({count: this.state.count + 1})} -> {() => setCount(count + 1)}
+
   // recipes 라는 변수 명으로 setRecipes 는 함수명으로 state 저장
   const [recipes, setRecipes] = useState([]) // chrome react 대브탭에 보면 hooks 내용으로 state 들어가 있음
+  // useState("...") 함수 안 아규먼트 값을 {recipes: ...} 값으로 넣어라
 
+
+  // 기존 componentDidMount, componentDidUpdate, componentWillUnmount  컴비네이션
   useEffect(() => {
     getRecipes()  
-  }, [])  // , [] 빈 배열 -> 마운트때 첫회만 실행. 배열 다른 값 -> 해당 변수가 변동될때마다 실행 
+  }, [])  // , [] 빈 배열 -> componentDidMount마운트때 첫회만 실행. 배열 다른 값 -> 해당 변수가 변동될때마다 실행 
 
   const getRecipes = async () => {
-    const res = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const res = await fetch(process.env.REACT_APP_API_URL);
     const data = await res.json();
     setRecipes(data.hits); // 받아온 데이터 배열 state 에 저장
+    // this.setState({recipes: data.hits}) 기존 스테이트
 
     console.log(data.hits);
     console.log(data.hits[0].recipe.source);
